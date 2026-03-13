139f78f800a9fa126e5a53cd7f943494c763342f
AstroP123 <leduyphuc2005@gmail.com>
2026-03-14 01:35:08 +0700

    home-page, PageController, SercurityConfig, and js for homepage

--- src/main/java/com/group/blog/BlogWebsiteApplication.java
+++ src/main/java/com/group/blog/BlogWebsiteApplication.java
@@ -3,11 +3,9 @@ package com.group.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 public class BlogWebsiteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BlogWebsiteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebsiteApplication.class, args);
+    }
 }
